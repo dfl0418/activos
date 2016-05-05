a41edbba1aa0ed269cca0e733fcfeef019dd1a95
dfl0418 <dlopez25@itfip.edu.co>
2016-05-05 11:13:31 -0500

    corrigiendo error migrate centro costos

--- database/migrations/2016_04_26_165852_CENTRO_COSTOS.php
+++ database/migrations/2016_04_26_165852_CENTRO_COSTOS.php
@@ -1,112 +1,33 @@
-<?php namespace Almacen\Http\Controllers;
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-use Almacen\Http\Requests;
-use Almacen\Http\Controllers\Controller;
-use Almacen\Http\Requests\CentroCostoForm;
+class CENTROCOSTOS extends Migration
+{
+/**
+* Run the migrations.
+*
+* @return void
+*/
+public function up()
+{
+Schema::create('centro_costos', function ($table) {
+$table->increments('id_centro_costo',10);
+$table->string('nombre_centro_costo', 45);
 
-use Illuminate\Http\Request;
-use Almacen\CentroCosto;
+$table->timestamps();
 
-
-
-class CentroCostoController extends Controller {
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-
-    public function index()
-    {
-        $centro_costos =CentroCosto::paginate(5);
-        return view("centrocosto.index")->with('centrocosto', $centro_costos);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        return view("centrocosto.create");
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return Response
-     */
-    public function store()
-    {
-        $centro_costos = new Categoria();
+});}
+/**
+* Reverse the migrations.
+*
+* @return void
+*/
+public function down()
+{
+//
 
 
-        $centro_costos->nombre_centrocosto = \Request::input('nombre_centrocosto');
-
-        $centro_costos->save();
-        return redirect('centrocosto/create')->with('message', 'centrocosto Creado');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        $centro_costos  = CentroCosto::find($id);
-        return view("centrocosto.create")->with('facultad',$centro_costos);
-
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        $centro_costos = CentroCosto::find($id);
-        $centro_costos->nombre_centrocosto = \Request::input('nombre_centrocosto');
-
-        $centro_costos->save();
-        return redirect()->route('centrocosto.index')->with('message', "Los datos de {$centro_costos->nombre} han sido actulizado");
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        $centro_costos = CentroCosto::find($id);
-        $centro_costos->delete();
-
-        return redirect()->route('centrocosto.index')->with('message', "facultad {$centro_costos->nombre} ha sido eliminado" );
-    }
-
+Schema::drop('centro_costos');
 }
-
+}
